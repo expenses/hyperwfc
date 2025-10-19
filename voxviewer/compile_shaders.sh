0e2eed66c6dea83bb9295a7878c857bf8bfb4778
Ashley Ruglys <ashley.ruglys@gmail.com>
2025-10-19 11:34:10 +0200

    Reduce to just wfc stuff

--- voxviewer/compile_shaders.sh
+++
@@ -1,4 +0,0 @@
-slang_command="slangc -DFALCOR_MATERIAL_INSTANCE_SIZE=128 -DFALCOR_NVAPI_AVAILABLE=0 -I Falcor/Source/Falcor -O3 -validate-uniformity"
-$slang_command src/shaders/blit_srgb.slang -o assets/shaders/blit_srgb.wgsl
-$slang_command -Wno-30081 -Wno-42050 src/shaders/raytrace.slang -entry main -o assets/shaders/raytrace.spv
-$slang_command -Wno-30081 -Wno-42050 src/shaders/raytrace.slang -entry main -o assets/shaders/raytrace.wgsl
